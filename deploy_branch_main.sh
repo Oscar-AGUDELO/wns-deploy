c792442c2113f971f4ab228b3026dcb0514a278e
Oscar-AGUDELO <oscar.agudelo.pro@gmail.com>
2023-03-29 09:47:09 +0000

    coucou 4

--- deploy_branch_main.sh
+++ deploy_branch_main.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-echo "PORT=$PORT"
 git fetch origin && git reset --hard origin/main && git clean -f -d
-GATEWAY_PORT=$PORT docker compose -f docker-compose.yml up --build -d
+GATEWAY_PORT=8000 docker compose -f docker-compose.yml up --build -d
